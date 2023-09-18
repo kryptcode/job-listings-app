6812440990720a61cb0d79f08531e4bb236a88ab
Koye <54333700+kryptcode@users.noreply.github.com>
2023-09-18 18:09:01 +0100

    Initial commit

---
+++ resources/views/listings.blade.php
@@ -0,0 +1,23 @@
+<h1>
+    {{-- <?php echo $heading; ?> without blade --}}
+    {{$heading}}
+</h1>
+
+@if (count($listings) == 0)
+    <p>No listings found</p>
+@endif
+
+{{-- <?php foreach($listings as $listing) : ?> without blade  --}}
+@foreach ( $listings as $listing )
+    
+<h2>
+    {{-- <?php echo $listing['title']; ?> --}}
+    {{$listing['title']}}
+</h2>
+<p>
+    {{-- <?php echo $listing['description']; ?> --}}
+    {{$listing['description']}}
+</p>
+
+@endforeach
+{{-- <?php endforeach; ?> without blade  --}}
